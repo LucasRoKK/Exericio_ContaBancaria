4b3624bb89e3e340cd6c7b84ac0acb5c6b3b2e5d
LucasRoKK <37778908+LucasRoKK@users.noreply.github.com>
2018-03-25 15:08:22 -0300

    Add files via upload

---
+++ ContaBancaria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_4
+{
+    class ContaBancaria
+    {
+        private int conta;
+        public string nome;
+        private double saldo;
+
+        public void Imprime()
+        {
+            Console.WriteLine("Conta atualizada");
+            Console.WriteLine("Conta: {0}, Titular: {1}, Saldo: {2}", this.conta, this.nome, this.saldo);
+            Console.ReadLine();
+        }
+
+        public ContaBancaria(int conta, string nome, double saldo)
+        {
+            this.conta = conta;
+            this.nome = nome;
+            this.saldo = saldo;
+        }
+
+        public ContaBancaria(int conta, string nome)
+        {
+            this.conta = conta;
+            this.nome = nome;
+            saldo = 0;
+        }
+
+        public void Deposito(double dep)
+        {
+            this.saldo += dep; 
+        }
+
+        public void Saque(double dep)
+        {
+            this.saldo -= (dep+5);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria banco;
+
+            Console.WriteLine("----BANCO JURUPIRA-----");
+            Console.WriteLine(" ");
+
+            Console.Write("Digite o numero da conta: ");
+            int conta;
+            conta = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial? (s/n)");
+            char validacao = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if(validacao == 's')
+            {
+                Console.Write("Digite o valor do deposíto inicial: ");
+                double valor;
+                valor = double.Parse(Console.ReadLine());
+                banco = new ContaBancaria(conta, nome, valor);
+            }
+            else
+            {
+                banco = new ContaBancaria(conta, nome);
+            }
+
+            Console.WriteLine();
+            banco.Imprime();
+
+            Console.Write("Digite um valor para deposito: ");
+            double aux = double.Parse(Console.ReadLine());
+            banco.Deposito(aux);
+            banco.Imprime();
+
+            Console.WriteLine("");
+
+            Console.Write("Digite um valor para saque: ");
+            aux = double.Parse(Console.ReadLine());
+            banco.Saque(aux);
+            banco.Imprime();
+
+        }
+    }
+}
